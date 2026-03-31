2a8b91b89ac27412f19cd5bc23a2e05d2016809e
MUEDSA <7676275+muedsa@users.noreply.github.com>
2026-03-31 18:09:35 +0800

    release: 0.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.muedsa.tvbox.lmm"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.0.7"
+        versionCode = 8
+        versionName = "0.0.8"
     }
 
     signingConfigs {
